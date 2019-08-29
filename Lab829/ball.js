b20f9c5e136a3508ece8c887d0f5da969ede04c9
Allison X. Smith <axsmith@WS-ACADBUS.EDU>
2019-08-29 08:49:37 -0700

    829

--- Lab829/ball.js
+++ Lab829/ball.js
@@ -1,10 +1,9 @@
-//doesmy h drive work
+
 class Ball{
   constructor(x,y,dx,dy){
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+    this.loc = createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.acc = createVector(-1, .1);
     this.clr = color(random(255), random(255), random(255))
   }
 
@@ -15,30 +14,31 @@ run(){
 }
 
 checkEdges(){
-  if(this.x<0){
-  this.dx = -this.dx
+  if(this.loc.x<0){
+  this.vel.x = -this.vel.x
   }
 
-  if(this.x>width){
-  this.dx = -this.dx
+  if(this.loc.x>width){
+  this.vel.x = -this.vel.x
   }
 
-  if(this.y<0){
-  this.dy = -this.dy
+  if(this.loc.y<0){
+  this.vel.y = -this.vel.y
   }
 
-  if(this.y>height){
-  this.dy = -this.dy
+  if(this.loc.y>height){
+  this.vel.y = -this.vel.y
   }
 }
 
 update(){
-  this.x = this.x + this.dx;
-  this.y = this.y + this.dy;
+  this.loc.add(this.vel.x);
+  this.loc.add(this.vel.y);
+  this.vel.add(this.acc);
 }
 
 render(){
   fill(this.clr);
-  ellipse(this.x, this.y, 50, 50);
+  ellipse(this.loc.x, this.loc.y, 50, 50);
   }
 }
