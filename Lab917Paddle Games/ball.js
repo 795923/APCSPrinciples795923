169e920dc099ac9887028f9e41d0905585d91fb4
Allison Smith <795923@seq.org>
2019-09-17 08:34:58 -0700

    paddle game

--- Lab917Paddle Games/ball.js
+++ Lab917Paddle Games/ball.js
@@ -1,12 +1,10 @@
 class Ball{
-  constructor(x,y,dx,dy,id){
+  constructor(x,y,dx,dy,i){
     this.loc = createVector(x,y);
     this.vel = createVector(dx,dy);
     this.acc = createVector(0, .2);
     this.clr = color(random(255), random(255), random(255))
-    this.id =id
-    this.health = 0
-    this.score = 0
+    this.i = 0
   }
 
   run(){
@@ -30,13 +28,13 @@ class Ball{
     }
 //score
     if(750<this.loc.y<800){
-      this.health= this.health - 1
-      // ball.splice(1,1,1)
+      health= health - 1
+      ball.splice(1,i)
     }
 //paddle
     if(this.loc.x > paddle.loc.x && this.loc.x < paddle.loc.x + paddle.w && this.loc.y > paddle.loc.y && this.loc.y < paddle.loc.y + paddle.h){
       this.vel.y = -this.vel.y
-      this.score= this.score + 1
+      score= score + 1
     }
   }
 
@@ -45,10 +43,10 @@ class Ball{
     this.loc.add(this.vel);
     fill(250, 0, 0);
     textSize(32);
-    text("Health: " + this.health, 10, 30);
+    text("Health: " + health, 10, 30);
     fill(0, 250, 0);
     textSize(32);
-    text("Score: " + this.score, 650, 30);
+    text("Score: " + score, 650, 30);
   }
 
   render(){
--- Lab917Paddle Games/sketch.js
+++ Lab917Paddle Games/sketch.js
@@ -1,8 +1,11 @@
 //  Allison Smith
-//  September 16, 2019
+//  September 17, 2019
 
 var ball = []
 var paddle = []
+var score = 0
+var health = 3
+var gameState = 1
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -11,13 +14,20 @@ function setup() {
 
 //  The draw function is called @ 30 fps
 function draw() {
-  background(20,20,20);
-  runObjects();
+  if(gameState === 1){
+    startGame()
+  }
+  else if(gameState === 2){
+      playGame()
+  }
+  else if(gameState === 3){
+      endGame()
+  }
 }
 
 function loadThings(n){
   for(var i=0; i < n; i++){
-    ball[i] = new Ball(random(width), 0, random(-8,8), random(-8,8), 1);
+    ball[i] = new Ball(random(width), 0, random(-8,8), random(-8,8), i);
   }
   paddle = new Paddle(random(width/2),725);
 }
@@ -27,4 +37,11 @@ function runObjects(){
   ball[i].run();
   }
   paddle.run();
+}
+function playGame(){
+  background(20,20,20);
+}
+
+function startGame(){
+    runObjects();
 }
