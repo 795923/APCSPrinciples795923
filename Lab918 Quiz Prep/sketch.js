eceba029b35180fa4fc36fdd5c77f702efbe16d2
Allison Smith <795923@seq.org>
2019-09-18 21:57:54 -0700

    quiz

---
+++ Lab918 Quiz Prep/sketch.js
@@ -0,0 +1,16 @@
+//Allison Smith
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+}
+var nums = [3, 4, 5, 6]
+for(var i = 0; i < nums.length; i++){
+   console.log(nums[i])
+}
+console.log("++++++++++++++++++++++++++")
+for(var i = nums.length-1; i >=0; i--){
+   console.log(i)
+}
