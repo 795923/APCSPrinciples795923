069c57a521a5068691addf615bba082be34f7638
Allison Smith <795923@seq.org>
2019-10-04 08:50:42 -0700

    sort

--- Lab104 MySort/sketch.js
+++ Lab104 MySort/sketch.js
@@ -1,13 +1,17 @@
-//  Your Name
-// 	Date or version number
-//  This is a comment
-//  The setup function function is called once when your program begins
+//  Allison Smith
+// 	10 / 4
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-
+  var list = [2,6,9,1,3,8]
+  swap(list, 0, 1)
+  functoin swap(list, a,b){
+    var temp =list[a];
+    list[a]= list[b];
+    list[b]= temp;
+  }
 
 }
 
