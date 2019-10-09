8f03bcfdf5317f7670bbdc20215bcfa7bfc87efe
Allison Smith <795923@seq.org>
2019-10-09 07:52:29 -0700

    checker

--- Lab1007 CheckerBoard/sketch.js
+++ Lab1007 CheckerBoard/sketch.js
@@ -13,15 +13,31 @@ function setup() {
   }
 }
 
+// function loadSquares() {
+// x=0
+// for (var i=0; i<=8; i= i+2){
+//   squares[i] = new Square(x,0,250,0,0)
+//   x= x+200
+//   }
+// x=100
+// for (var i=1; i<=8; i= i+2){
+//   squares[i] = new Square(x,0,0,250,0)
+//   x= x+200
+//   }
+// }
 function loadSquares() {
-x=0
-for (var i=0; i<=8; i= i+2){
-  squares[i] = new Square(x,0,250,0,0)
-  x= x+200
-  }
-x=100
-for (var i=1; i<=8; i= i+2){
-  squares[i] = new Square(x,0,0,250,0)
-  x= x+200
+  x=0
+  y=0
+  for(var j = 0;  j<=8; j++ ){
+    for(var i=0; i<=8; i++){
+      if(i%2 === 0){
+        squares[i] = new Square(x,y,250,0,0)
+        x= x+100
+      }else if (i%2 === 1) {
+        squares[i] = new Square(x,y,0,250,0)
+        x= x+100
+      }
+    }
+    y= y+100
   }
 }
---
+++ Lab1009 BubbleSort/sketch.js
@@ -0,0 +1,39 @@
+//  Allison Smith
+// 	October 6, 2019
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  var list = [2,6,9,1,3,8]
+  function swap(list, a,b){
+    var temp =list[a];
+    list[a]= list[b];
+    list[b]= temp;
+  }
+  perfect = list[0] > list[1] > list[2] > list[4] > list[5] > list[6]
+  function test(){
+    if (perfect === false){
+      if (list[0] > list[1]){
+        swap(list, 0, 1)
+      }
+      if (list[1] > list[2]){
+        swap(list, 1, 2)
+      }
+      if (list[2] > list[3]){
+        swap(list, 2, 3)
+      }
+      if (list[3] > list[4]){
+        swap(list, 3, 4)
+      }
+      if (list[4] > list[5]){
+        swap(list, 4, 5)
+      }
+      if (list[5] > list[6]){
+        swap(list, 5, 6)
+      }
+    }
+  }
+  test(1);
+  console.log(list)
+}
