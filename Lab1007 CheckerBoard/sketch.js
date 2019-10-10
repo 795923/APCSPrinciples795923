9d2c7136946cf8f3dc7beff8965b6a00833f6a5f
Allison Smith <795923@seq.org>
2019-10-10 08:45:09 -0700

    finished board

--- Lab1007 CheckerBoard/sketch.js
+++ Lab1007 CheckerBoard/sketch.js
@@ -1,5 +1,5 @@
 //  Allison Smith
-//  October 8, 2019
+//  October 10, 2019
 var squares = []
 
 function setup() {
@@ -8,53 +8,34 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
   loadSquares();
-  for (var i=0; i<squares.length; i++){
-  squares[i].render();
-  }
 }
 
-// function loadSquares() {
-// x=0
-// for (var i=0; i<=8; i= i+2){
-//   squares[i] = new Square(x,0,250,0,0)
-//   x= x+200
-//   }
-// x=100
-// for (var i=1; i<=8; i= i+2){
-//   squares[i] = new Square(x,0,0,250,0)
-//   x= x+200
-//   }
-// }
-
-// function loadSquares() {
-//   x=0
-//   y=0
-//   for(var j = 0;  j<=8; j++ ){
-//     for(var i=0; i<=8; i++){
-//       if(i%2 === 0){
-//         squares[i] = new Square(x,y,250,0,0)
-//         x= x+100
-//       }else if (i%2 === 1) {
-//         squares[i] = new Square(x,y,0,250,0)
-//         x= x+100
-//       }
-//     }
-//     y= y+100
-//   }
-// }
-
 function loadSquares() {
-  x=0
-  y=0
   for(var j = 0; j<8; j++){
+    if(j%2 === 0){
+      for(var i=0; i<8; i++){
+        if(i%2 === 0){
+          squares[i] = new Square(i*100,j*100,250,0,0)
+        }
+        else if(i%2 === 1) {
+          squares[i] = new Square(i*100,j*100,0,250,0)
+        }
+      }
+      for (var a=0; a<squares.length; a++){
+      squares[a].render();
+      }
+    }
+  if(j%2 === 1){
     for(var i=0; i<8; i++){
       if(i%2 === 0){
-        x=i*100
-        squares[i] = new Square(x,y,250,0,0)
-      }
+        squares[i] = new Square(i*100,j*100,0,250,0)
+        }
       else if(i%2 === 1) {
-        x=i*100
-        squares[i] = new Square(x,y,0,250,0)
+        squares[i] = new Square(i*100,j*100,250,0,0)
+        }
+      }
+      for (var a=0; a<squares.length; a++){
+      squares[a].render();
       }
     }
   }
