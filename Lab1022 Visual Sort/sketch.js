1d74b24819d04cb1e4204209b1917b57fb1a56c5
Allison Smith <795923@seq.org>
2019-10-28 08:51:08 -0700

    sort

--- Lab1022 Visual Sort/sketch.js
+++ Lab1022 Visual Sort/sketch.js
@@ -4,6 +4,7 @@ var rectangles = [];
 var heights = [];
 var rectX = 0;
 var numRects = 20;
+var horizLoc = [];
 
 function setup() {
   var cnv = createCanvas(800, 800);
@@ -11,9 +12,10 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
   for(var i=0; i < numRects; i++){
-    var rectHeight = random(10,height);
+    var rectHeight = random(10, height);
     rectangles[i] = new Rectangle(rectHeight, rectX, width/numRects);
     heights.push(rectHeight);
+    horizLoc.push(rectX);
     rectX = rectX + (width/numRects);
   }
   for(var i=0; i < numRects; i++){
@@ -24,10 +26,17 @@ function setup() {
 function draw(){
 //insert
   for(var j = 0; j < heights.length; j++){
-    for(var i = j; i > 0; i--){
+    for(var i = j; i < heights.length; i++){
       if (heights[i] < heights[i-1]){
-        swap(heights, i, i-1)
+        var tmp = heights[i];
+        rectangles[i] = new Rectangle(heights[i+1], horizLoc[i], width/numRects)
+        rectangles[i+1] = new Rectangle(heights[i], horizLoc[i+1], width/numRects)
+        for (n=0; n< numRects; n++){
+          rectangles[n].render();
+        }
+        heights[i] = heights[i+1]
+        heights[i+1] = tmp
+        }
         }
       }
     }
-  }
