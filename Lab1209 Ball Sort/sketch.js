0d12e3b15de64e6c08de56614f28374bf79ae7f8
Allison Smith <795923@seq.org>
2019-12-09 20:36:52 -0800

    reds don't show up in order

--- Lab1209 Ball Sort/sketch.js
+++ Lab1209 Ball Sort/sketch.js
@@ -4,18 +4,16 @@ var balls = [];
 var colors = [];
 var numBalls = 20;
 
-
 function setup() {
-  frameRate(5);
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
   for(var i=0; i < numBalls; i++){
-      balls[i] = new Ball(i*40,650);
+      balls[i] = new Ball(i*40,150);
     }
-  for (j=0; j<balls.length-1; j++){
-    for (i=0; i<balls.length-1-j; i++){
+  for (j=0; j<numBalls-1; j++){
+    for (i=0; i<numBalls-1-j; i++){
       if (balls[i+1].redClr < balls[i].redClr) {
         var tmp = balls[i].redClr;
         balls[i].redClr = balls[i+1].redClr;
